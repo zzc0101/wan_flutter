260567fbfa83d433dfebb3dd6b8b8e6191a825c1
zzc0101 <318185326@qq.com>
2025-11-15 14:43:46 +0800

    udpate

--- lib/pages/auth/register_page.dart
+++ lib/pages/auth/register_page.dart
@@ -29,53 +29,58 @@ class _RegisterPageState extends State<RegisterPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.teal,
-      body: Container(
-        padding: EdgeInsets.only(left: 20.w, right: 20.w),
-        alignment: Alignment.center,
-        child: Consumer<AuthViewModel>(
-          builder: (context, vm, child) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                commonInput(
-                  labelText: "输入账号",
-                  onChanged: (value) {
-                    vm.registerInfo.name = value;
-                  },
-                ),
-                SizedBox(height: 20.h),
-                commonInput(
-                  labelText: "输入密码",
-                  obscureText: true,
-                  onChanged: (value) {
-                    vm.registerInfo.password = value;
-                  },
-                ),
-                SizedBox(height: 20.h),
-                commonInput(
-                  labelText: "再次输入密码",
-                  obscureText: true,
-                  onChanged: (value) {
-                    vm.registerInfo.rePassword = value;
-                  },
-                ),
-                SizedBox(height: 50.h),
-                whiteBorderButton(
-                  title: "点我开始注册",
-                  onTap: () {
-                    viewModel.register().then((value) {
-                      if (value == true) {
-                        showToast("注册成功");
-                        RouteUtils.pop(context);
-                      }
-                    });
-                  },
-                ),
-              ],
-            );
-          },
+    return ChangeNotifierProvider<AuthViewModel>(
+      create: (context) {
+        return viewModel;
+      },
+      child: Scaffold(
+        backgroundColor: Colors.teal,
+        body: Container(
+          padding: EdgeInsets.only(left: 20.w, right: 20.w),
+          alignment: Alignment.center,
+          child: Consumer<AuthViewModel>(
+            builder: (context, vm, child) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  commonInput(
+                    labelText: "输入账号",
+                    onChanged: (value) {
+                      vm.registerInfo.name = value;
+                    },
+                  ),
+                  SizedBox(height: 20.h),
+                  commonInput(
+                    labelText: "输入密码",
+                    obscureText: true,
+                    onChanged: (value) {
+                      vm.registerInfo.password = value;
+                    },
+                  ),
+                  SizedBox(height: 20.h),
+                  commonInput(
+                    labelText: "再次输入密码",
+                    obscureText: true,
+                    onChanged: (value) {
+                      vm.registerInfo.rePassword = value;
+                    },
+                  ),
+                  SizedBox(height: 50.h),
+                  whiteBorderButton(
+                    title: "点我开始注册",
+                    onTap: () {
+                      vm.register().then((value) {
+                        if (value == true) {
+                          showToast("注册成功");
+                          RouteUtils.pop(context);
+                        }
+                      });
+                    },
+                  ),
+                ],
+              );
+            },
+          ),
         ),
       ),
     );
